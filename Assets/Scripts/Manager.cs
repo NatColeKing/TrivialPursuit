8ffa7f89eed9d5ed0e70a2cc77a1cde3f0cdd51d
flamegiraffe <flamegiraffes@gmail.com>
2018-05-29 19:37:59 -0700

    Scene manager added, lighting changed

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Manager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if(Time.time > 13)
+		{
+			SceneManager.LoadScene("02 - Trip and Cry");
+		}
+		// if(Time.time > 17)
+		// {
+		// 	UnityEditor.EditorApplication.isPlaying = false;
+		// }
+		// Application.Quit();
+
+	}
+}
