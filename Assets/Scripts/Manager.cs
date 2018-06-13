9cf3b9cdc77ce44bfbcbb570cdb9f60887982c1e
Ellen Tseng <33066419+elltseng@users.noreply.github.com>
2018-06-13 16:05:34 -0700

    Scene 5 camera

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -6,59 +6,46 @@ using UnityEngine.SceneManagement;
 
 public class Manager : MonoBehaviour {
 	int scene = 0;
-	void Start ()
-	{
+	void Start (){
 		DontDestroyOnLoad(this.gameObject);
 	}
 
 	// Update is called once per frame
-	void Update ()
-	{
+	void Update (){
 		Debug.Log("Scene = " + scene);
 		Debug.Log("Time = " + Time.timeSinceLevelLoad);
-		if(scene == 5)
-		{
-			if(Time.timeSinceLevelLoad > 6)
-			{
+
+		if(scene == 5){
+			if(Time.timeSinceLevelLoad > 6){
 				// Application.Quit();
 			}
 		}
-		if(scene == 4)
-		{
-			if(Time.timeSinceLevelLoad > 62)
-			{
+		if(scene == 4){
+			if(Time.timeSinceLevelLoad > 62){
 				SceneManager.LoadScene("05 - End");
 				scene = 5;
 			}
 		}
-		if(scene == 3)
-		{
-			if(Time.timeSinceLevelLoad > 4)
-			{
+		if(scene == 3){
+			if(Time.timeSinceLevelLoad > 4){
 				SceneManager.LoadScene("04 - Stomp to fight");
 				scene = 4;
 			}
 		}
-		if(scene == 2)
-		{
-			if(Time.timeSinceLevelLoad > 20)
-			{
+		if(scene == 2){
+			if(Time.timeSinceLevelLoad > 20){
 				SceneManager.LoadScene("03 - Blackout");
 				scene = 3;
 			}
 		}
-		if(scene == 1)
-		{
-			if(Time.timeSinceLevelLoad > 12)
-			{
+		if(scene == 1){
+			if(Time.timeSinceLevelLoad > 12){
 				SceneManager.LoadScene("02 - Trip and Cry");
 				scene = 2;
 			}
 		}
-		if(scene == 0)
-		{
-			if(Time.timeSinceLevelLoad > 9)
-			{
+		if(scene == 0){
+			if(Time.timeSinceLevelLoad > 9){
 				SceneManager.LoadScene("01 - Running");
 				scene = 1;
 			}
---
+++ Assets/Scripts/Scene 04/bandit_02_scene04.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bandit_02_scene04 : MonoBehaviour {
+
+	AudioSource source;
+	// Use this for initialization
+	void Start () {
+		source = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log("Time = " + Time.timeSinceLevelLoad);
+
+		if(Time.timeSinceLevelLoad > 17){
+				source.Play();
+		}
+	}
+}
